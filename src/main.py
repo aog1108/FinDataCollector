a909da34a6c025d97483e1518e8f653c8952578c
aog1108 <aog941108@gmail.com>
2024-07-31 23:30:03 +0900

    Add main function input

--- src/main.py
+++ src/main.py
@@ -5,26 +5,49 @@ import logging
 import Logger
 from BatchConfig import *
 import sys
+import os
 
 today = datetime.today()
 
-if datetime.now().hour < 12:
-    batch_config = SODBatchConfig()
-    one_day_shift = relativedelta(days=1)
-    batch_date = today - one_day_shift
+if len(sys.argv) > 1:
+    batch_date = datetime.strptime(sys.argv[1], '%Y%m%d')
+
+if len(sys.argv) > 2:
+    batch_name = sys.argv[2]
 else:
+    if datetime.now().hour < 12:
+        batch_name = 'SOD'
+        one_day_shift = relativedelta(days=1)
+        batch_date = today - one_day_shift
+    else:
+        batch_name = 'EOD'
+        batch_date = today
+
+if batch_name == 'SOD':
+    batch_config = SODBatchConfig()
+
+    # time synchronization
+    os.system('net stop w32time')
+    os.system('w32tm /unregister')
+    os.system('w32tm /register')
+    os.system('net start w32time')
+    os.system('w32tm /resync')
+
+elif batch_name == 'EOD':
     batch_config = EODBatchConfig()
-    batch_date = today
+
+else:
+    raise RuntimeError('Invalid batch type')
 
 batch_config_name = type(batch_config).__name__
 batch_date_str = batch_date.strftime('%Y-%m-%d')
 
 batch_handler = DataBatch.BatchHandler(batch_config)
 
-if today.weekday() in (0, 6) and batch_config_name == 'SODBatchConfig':
+if len(sys.argv) == 1 and today.weekday() in (0, 6) and batch_config_name == 'SODBatchConfig':
     batch_handler.set_batch_function_flag('do_equity_index_data_batch', 'Off')
 
-if today.weekday() in (5, 6) and batch_config_name == 'EODBatchConfig':
+if len(sys.argv) == 1 and today.weekday() in (5, 6) and batch_config_name == 'EODBatchConfig':
     sys.exit()
 
 # batch_handler.set_batch_function_flag('do_equity_index_data_batch', 'Off')
@@ -32,8 +55,8 @@ if today.weekday() in (5, 6) and batch_config_name == 'EODBatchConfig':
 # batch_handler.set_batch_function_flag('do_krx_stock_data_batch', 'Off')
 # batch_handler.set_batch_function_flag('do_insert_and_delete_krx_listed_stocks_info_batch', 'Off')
 # batch_handler.set_batch_function_flag('do_synchronize_krx_stocks_data_ticker_batch', 'Off')
-# batch_handler.set_batch_function_flag('do_insert_and_delete_krx_delisted_stocks_info_batch', 'Off')
-# batch_handler.set_batch_function_flag('do_update_krx_listed_stocks_info_batch', 'Off')
+batch_handler.set_batch_function_flag('do_insert_and_delete_krx_delisted_stocks_info_batch', 'Off')
+# batch_handler.set_batch_function_flag('do_update_krx_listed_stocks_info_batch', 'On')
 # batch_handler.set_batch_status_insert_on(False)
 
 if __name__ == '__main__':
